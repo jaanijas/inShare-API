284030900c70e2f9117fc18b0ff0935a50bb53cd
unknown <janijaswant3@gmail.com>
2022-10-20 13:13:16 +0530

    Initial

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('views',path.join(__dirname,'/views'));
+app.set('view engine', 'ejs');
+
+const PORT=process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
+const connectDB = require('./config/db');
+connectDB();
+
+//Routes
+
+app.use('/api/files', require('./routes/files'));
+app.use('/files',require('./routes/show'));
+app.use('/files/download',require('./routes/download'));
+
+app.listen(PORT,()=>{
+    console.log('listening on port'+` ${PORT}`);
+});
